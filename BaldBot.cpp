ab4c5aa0ef70346027937ddf76c9a9aaa5f6629b
Alexander Turenko <alextux@bk.ru>
2012-04-05 22:05:43 +0400

    Write almost full game cycle for BaldBot.

--- BaldBot.cpp
+++ BaldBot.cpp
@@ -2,7 +2,8 @@
 #include "Socket.hpp"
 #include "ServerMsgLexer.hpp"
 
-#define MSG(str) (str), sizeof(str)
+#define MSG(str) (str), (sizeof(str) - 1)
+#define MIN(n1, n2) (((n1) <= (n2)) ? (n1) : (n2))
 
 static const int bufferSize = 4096;
 
@@ -16,7 +17,7 @@ struct BotState {
        : socket(host, port), readed(0) {}
 };
 
-ServerMsg* expectMsg(BotState& state, TypeOfServerMsg type, bool skipIfNotOk)
+ServerMsg* expectMsg(BotState& state, TypeOfServerMsg type, bool skipFailed)
 {
     ServerMsg *msg = 0;
 
@@ -31,14 +32,14 @@ ServerMsg* expectMsg(BotState& state, TypeOfServerMsg type, bool skipIfNotOk)
             } else {
 //                state.lexer.putEOF();
                 printf("Got EOF, exiting...");
-                exit(0);
+                throw 2;
             }
 
             msg = state.lexer.getMsg();
         }
 
         msg->print(stdout);
-    } while ((skipIfNotOk && !msg->ok) || (msg->type != type));
+    } while ((skipFailed && !msg->ok) || (msg->type != type));
 
     if (msg->ok)
         return msg;
@@ -61,7 +62,7 @@ int main(int argc, char** argv)
         get("buy-ok");
         send("sell"); // min market and productions
         get("sell-ok");
-        send("make"); // min raws and productions
+        send("make"); // min raws and factories
         get("make-ok");
         send("turn");
         get("turn-ok");
@@ -73,9 +74,63 @@ int main(int argc, char** argv)
         BotState state(argv[1], argv[2]);
         ServerMsg *msg;
         state.socket.connect();
+
         state.socket.write(MSG("join\r\n"));
         msg = expectMsg(state, MSG_JOIN_RESPONCE, false);
         msg = expectMsg(state, MSG_ROUNDS_ASYNC, true);
+
+        do {
+            state.socket.write(MSG("status --market\r\n"));
+            msg = expectMsg(state, MSG_STATUS_RESPONCE, false);
+
+            int marketRaws = msg->status->getMarketRaws();
+            int marketRawMinPrice = msg->status->getMarketRawMinPrice();
+            int marketProductions = msg->status->getMarketProductions();
+            int marketProdMaxPrice = msg->status->getMarketProdMaxPrice();
+
+            state.socket.write(MSG("status\r\n"));
+            msg = expectMsg(state, MSG_STATUS_RESPONCE, false);
+
+            int playerMoney = msg->status->getPlayerMoney();
+            int playerRaws = msg->status->getPlayerRaws();
+            int playerProductions = msg->status->getPlayerProductions();
+            int playerFactories = msg->status->getPlayerFactories();
+
+            int count = MIN(marketRaws, playerFactories);
+            int cost = marketRawMinPrice;
+
+            char *str;
+            asprintf(&str, "buy %d %d\r\n", count, cost);
+            state.socket.write(str, strlen(str));
+            free(str);
+
+            msg = expectMsg(state, MSG_BUY_RESPONCE, false);
+
+            count = MIN(marketProductions, playerProductions);
+            cost = marketProdMaxPrice;
+
+            asprintf(&str, "sell %d %d\r\n", count, cost);
+            state.socket.write(str, strlen(str));
+            free(str);
+
+            msg = expectMsg(state, MSG_SELL_RESPONCE, false);
+
+            count = MIN(playerRaws, playerFactories);
+
+            asprintf(&str, "make %d\r\n", count);
+            state.socket.write(str, strlen(str));
+            free(str);
+
+            msg = expectMsg(state, MSG_MAKE_RESPONCE, false);
+
+            state.socket.write(MSG("turn\r\n"));
+            /* TODO: only responce, not async;
+             * true in 3 argument in temporally. */
+            msg = expectMsg(state, MSG_TURN_RESPONCE, true);
+
+            msg = expectMsg(state, MSG_MONTH_COMPLETED, false);
+        } while (1);
+
         state.socket.disconnect();
 //        lexer.putEOF();
     } catch(Exception& ex) {
