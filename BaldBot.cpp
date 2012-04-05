6e9ab36888f90bd0dae48109c1eee1116e23036a
Alexander Turenko <alextux@bk.ru>
2012-04-05 12:21:08 +0400

    Add method for wait certain message type for BaldBot.

--- BaldBot.cpp
+++ BaldBot.cpp
@@ -2,17 +2,45 @@
 #include "Socket.hpp"
 #include "ServerMsgLexer.hpp"
 
+#define MSG(str) (str), sizeof(str)
+#define BUFFER_SIZE 4
+
+struct BotState {
+   char buffer[BUFFER_SIZE];
+   ServerMsgLexer lexer;
+   Socket socket;
+   int readed;
+
+   BotState(const char* host, const char* port)
+       : socket(host, port), readed(0) {}
+};
+
+ServerMsg* expectMsg(BotState& state, TypeOfServerMsg type)
+{
+    ServerMsg *msg = 0;
+
+    do {
+        msg = state.lexer.getMsg();
+
+        while (msg == 0) {
+            state.socket.read(state.buffer, BUFFER_SIZE * sizeof(char*), &(state.readed));
+            state.lexer.putNewData(state.buffer, state.readed);
+            msg = state.lexer.getMsg();
+        }
+    } while (msg->type != type);
+
+    if (msg->okResponce)
+        return msg;
+
+    throw 1; /* TODO */
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 3) {
         fprintf(stderr, "Usage: ./BaldBot host port\n");
         return 1;
     }
-
-    ServerMsgLexer lexer;
-    Socket s(argv[1], argv[2]);
-    char buffer[4096];
-    int readed;
 
 #if 0
     send("join");
@@ -31,11 +59,12 @@ int main(int argc, char** argv)
 #endif
 
     try {
-        s.connect();
-        while(s.read(buffer, sizeof(buffer), &readed)) {
-            lexer.putNewData(buffer, readed);
-        } while (1);
-        s.disconnect();
+        BotState state(argv[1], argv[2]);
+        ServerMsg *msg;
+        state.socket.connect();
+        state.socket.write(MSG("join\r\n"));
+        msg = expectMsg(state, MSG_JOIN_RESPONCE);
+        state.socket.disconnect();
 //        lexer.putEOF();
     } catch(Exception& ex) {
         printf("%s", ex.toString());
